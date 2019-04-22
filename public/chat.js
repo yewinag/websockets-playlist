80ba1a51fda027fd785df689c55d90be9da9835c
Ye Win Aung <yewinaung@rebbiz.com>
2019-04-22 09:26:01 +0630

    initial commit

---
+++ public/chat.js
@@ -0,0 +1,29 @@
+// Make connection
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
+
+// Emit events
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+  });
+  message.value = "";
+});
+
+var video = document.getElementById('play_video');
+
+video.addEventListener('click', function(e){    
+  socket.emit('chat', {
+    hell: 'world'
+  })
+})
+
+socket.on('chat', function(data){  
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
